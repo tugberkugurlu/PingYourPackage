07765c7c3a37280a52047ca65c979125a3c03aa1
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-08-06 18:49:32 +0300

    registered PingYourPackageAuthHandler

--- src/apps/PingYourPackage.API.WebHost/App_Start/WebAPIConfig.cs
+++ src/apps/PingYourPackage.API.WebHost/App_Start/WebAPIConfig.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using PingYourPackage.API.MessageHandlers;
 
 namespace PingYourPackage.API.WebHost {
 
     public class WebAPIConfig {
 
         public static void Configure(HttpConfiguration config) { 
+
+            //Message Handlers
+            config.MessageHandlers.Add(new PingYourPackageAuthHandler());
+
+            //Formatters
+
+            //Default Services
         }
     }
 }
