4002264266d96edf877982043aae452b10a3406a
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-08-06 18:42:31 +0300

    added abstract BasicAuthenticationHandler class and PingYourPackageAuthHandler

---
+++ src/apps/PingYourPackage.API/MessageHandlers/PingYourPackageAuthHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using PingYourPackage.Domain.Services;
+
+namespace PingYourPackage.API.MessageHandlers {
+
+    public class PingYourPackageAuthHandler : BasicAuthenticationHandler {
+
+        protected override IPrincipal AuthenticateUser(HttpRequestMessage request, string username, string password, System.Threading.CancellationToken cancellationToken) {
+
+            var membershipService = (IMembershipService)request.GetDependencyScope().GetService(typeof(IMembershipService));
+            return membershipService.ValidateUser(username, password);
+        }
+    }
+}
