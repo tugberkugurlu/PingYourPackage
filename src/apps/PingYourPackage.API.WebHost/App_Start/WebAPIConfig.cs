75902183d6ac5828201d157c80e380634d7f0556
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-08-07 11:09:04 +0300

    added DefaultContentNegotiator with excludeMatchOnTypeOnly as true to send back 406 if the type does not match

--- src/apps/PingYourPackage.API.WebHost/App_Start/WebAPIConfig.cs
+++ src/apps/PingYourPackage.API.WebHost/App_Start/WebAPIConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web;
 using System.Web.Http;
 using PingYourPackage.API.MessageHandlers;
@@ -20,6 +21,16 @@ namespace PingYourPackage.API.WebHost {
             config.Filters.Add(new AuthorizeAttribute());
 
             //Default Services
+
+            //NOTE: This will come with RTM
+            // From DefaultContentNegotiator class:
+            // If ExcludeMatchOnTypeOnly is true then we don't match on type only which means
+            // that we return null if we can't match on anything in the request. This is useful
+            // for generating 406 (Not Acceptable) status codes.
+            //config.Services.Replace(
+            //    typeof(IContentNegotiator),
+            //    new DefaultContentNegotiator(excludeMatchOnTypeOnly: true)
+            //);
         }
     }
 }
