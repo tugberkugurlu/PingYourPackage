e20376d10dc87b3b64cc1e2fc32fed06ebdbff46
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-08-14 18:21:25 +0300

    added ShipmentRepositoryExtensions

---
+++ src/apps/PingYourPackage.Domain/Entities/Extensions/ShipmentRepositoryExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingYourPackage.Domain.Entities {
+    
+    public static class ShipmentRepositoryExtensions {
+
+        public static IQueryable<Shipment> GetShipmentsByAffiliateKey(
+            this IEntityRepository<Shipment> shipmentRepository, Guid affiliateKey) {
+            
+            return shipmentRepository.GetAll().Where(x => x.AffiliateKey == affiliateKey);
+        }
+
+
+        public static IQueryable<Shipment> GetNotDeliveredShipments(
+            this IEntityRepository<Shipment> shipmentRepository) {
+
+                var shipmenents = from shipment in shipmentRepository.GetAll()
+                                  where shipment.ShipmentStates.Any(x => 
+                                      x.ShipmentStatus != ShipmentStatus.Delivered)
+                                  select shipment;
+
+                return shipmenents;
+        }
+
+        public static IQueryable<Shipment> GetNotDeliveredShipments(
+            this IEntityRepository<Shipment> shipmentRepository, Guid affiliateKey) {
+
+                return shipmentRepository.GetNotDeliveredShipments().Where(x => 
+                    x.AffiliateKey == affiliateKey);
+        }
+
+    }
+}
