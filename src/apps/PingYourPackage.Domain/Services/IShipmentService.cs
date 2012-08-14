34ba46ce1e4b68f8cf65e0399ba8a30c086e4b46
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-08-14 18:07:28 +0300

    added IShipmentService

---
+++ src/apps/PingYourPackage.Domain/Services/IShipmentService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PingYourPackage.Domain.Entities;
+
+namespace PingYourPackage.Domain.Services {
+    
+    public interface IShipmentService {
+
+        void AddShipment(Affiliate affiliate, Shipment shipment);
+        void ChangeShipmentState(Guid shipmentKey, ShipmentStatus shipmentStatus);
+    }
+}
