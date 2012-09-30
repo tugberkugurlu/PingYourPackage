77419f91bb3d6facfd6f1e04d06cdf89eb93e772
tugberkugurlu <tugberkugurlu@gmail.com>
2012-09-30 08:13:01 +0300

    updated from RC to RTM

---
+++ src/apps/PingYourPackage.Domain/Services/CryptoService (TugberkWin8's conflicted copy 2012-09-21).cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingYourPackage.Domain.Services {
+
+    public class CryptoService : ICryptoService {
+
+        public string GenerateSalt() {
+
+            var data = new byte[0x10];
+            using (var cryptoServiceProvider = new RNGCryptoServiceProvider()) {
+
+                cryptoServiceProvider.GetBytes(data);
+                return Convert.ToBase64String(data);
+            }
+        }
+
+        public string EncryptPassword(string password) {
+
+            return EncryptPassword(password, GenerateSalt());
+        }
+
+        public string EncryptPassword(string password, string salt) {
+
+            if (string.IsNullOrEmpty(password)) {
+
+                throw new ArgumentNullException("password");
+            }
+
+            if (string.IsNullOrEmpty(salt)) {
+
+                throw new ArgumentNullException("salt");
+            }
+
+            using (var sha256 = SHA256.Create()) {
+
+                //TODO: This is wrong.
+                //      Pass the salted parameter here instead of generating one.
+                var saltedPassword = string.Format("{0}{1}", GenerateSalt(), password);
+   
+                byte[] saltedPasswordAsBytes = Encoding.UTF8.GetBytes(saltedPassword);
+                return Convert.ToBase64String(sha256.ComputeHash(saltedPasswordAsBytes));
+            }
+        }
+    }
+}
