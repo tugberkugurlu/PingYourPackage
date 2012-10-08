9ce58a7429a85e9393e9467ba8833b6b7cab0518
tugberkugurlu <tugberkugurlu@gmail.com>
2012-10-08 13:16:20 +0300

    added resources.txt

--- src/apps/PingYourPackage.API/Config/RouteConfig.cs
+++ src/apps/PingYourPackage.API/Config/RouteConfig.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Http;
+using System.Web.Http.Routing;
 
 namespace PingYourPackage.API.Config {
 
@@ -11,10 +13,30 @@ namespace PingYourPackage.API.Config {
         public static void RegisterRoutes(HttpRouteCollection routes) {
 
             routes.MapHttpRoute(
+                "UsersPatchHttpRoute",
+                "api/users/{id}/{action}",
+                defaults: new { controller = "useroperations" },
+                constraints: new { httpMethod = new HttpMethodConstraint(new HttpMethod("PATCH")) }
+            );
+
+            routes.MapHttpRoute(
                 "DefaultHttpRoute",
                 "api/{controller}/{id}",
                 new { id = RouteParameter.Optional }
             );
+        }
+    }
+
+    public class UserOperationsController : ApiController {
+
+        public HttpResponseMessage Status(StatusRequestCommand cmd) {
+
+            throw new NotImplementedException();
+        }
+
+        public HttpResponseMessage Roles(RolesRequestCommand cmd) {
+
+            throw new NotImplementedException();
         }
     }
 }
