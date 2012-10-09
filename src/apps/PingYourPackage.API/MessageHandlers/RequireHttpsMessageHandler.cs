674d3b94f9e2dafebe41e41f5f84edaba092cdf6
tugberkugurlu <tugberkugurlu@gmail.com>
2012-10-09 16:36:44 +0300

    added RequireHttpsMessageHandler and RequireHttpsMessageHandlerTest

---
+++ src/apps/PingYourPackage.API/MessageHandlers/RequireHttpsMessageHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PingYourPackage.API.MessageHandlers {
+
+    public class RequireHttpsMessageHandler : DelegatingHandler {
+
+        protected override Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request, 
+            CancellationToken cancellationToken) {
+
+            if (request.RequestUri.Scheme != Uri.UriSchemeHttps) {
+
+                HttpResponseMessage forbiddenResponse = 
+                    request.CreateResponse(HttpStatusCode.Forbidden);
+
+                forbiddenResponse.ReasonPhrase = "SSL Required";
+                return Task.FromResult<HttpResponseMessage>(forbiddenResponse);
+            }
+
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
---
+++ src/test/PingYourPackage.API.Test/MessageHandlers/RequireHttpsMessageHandlerTest.cs
@@ -0,0 +1,41 @@
+﻿using PingYourPackage.API.MessageHandlers;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PingYourPackage.API.Test.MessageHandlers {
+    
+    public class RequireHttpsMessageHandlerTest {
+
+        [Fact]
+        public async Task Returns_Forbidden_If_Request_Is_Not_Over_HTTPS() {
+            
+            //Arange
+            var request = new HttpRequestMessage(
+                HttpMethod.Get, "http://localhost:8080");
+
+            //Act
+            var response = await TestHelper.InvokeMessageHandler(request,
+                new RequireHttpsMessageHandler());
+
+            Assert.Equal(
+                HttpStatusCode.Forbidden, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Returns_Delegated_StatusCode_When_Request_Is_Over_HTTPS() {
+
+            //Arange
+            var request = new HttpRequestMessage(
+                HttpMethod.Get, "https://localhost:8080");
+
+            //Act
+            var response = await TestHelper.InvokeMessageHandler(request, 
+                new RequireHttpsMessageHandler());
+
+            Assert.Equal(
+                HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
