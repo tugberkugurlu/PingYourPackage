0577c68c931a6b7742d7a23784d402f722a6275b
tugberkugurlu <tugberkugurlu@gmail.com>
2012-10-22 10:32:05 +0300

    completed ShipmentsControllerIntegrationTest.PutShipment tets

--- src/test/PingYourPackage.API.Test.Integration/Controllers/ShipmentsControllerIntegrationTest.cs
+++ src/test/PingYourPackage.API.Test.Integration/Controllers/ShipmentsControllerIntegrationTest.cs
@@ -2,19 +2,25 @@
 using Autofac.Integration.WebApi;
 using Moq;
 using PingYourPackage.API.Model.Dtos;
+using PingYourPackage.API.Model.RequestModels;
 using PingYourPackage.Domain.Entities;
 using PingYourPackage.Domain.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Net.Http.Formatting;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http;
 using Xunit;
 
 namespace PingYourPackage.API.Test.Integration.Controllers {
     
     public class ShipmentsControllerIntegrationTest {
+
+        private static readonly string ApiBaseRequestPath = "api/shipments";
 
         public class GetShipments {
 
@@ -31,7 +37,7 @@ namespace PingYourPackage.API.Test.Integration.Controllers {
                         httpMethod: HttpMethod.Get,
                         uri: string.Format(
                             "https://localhost/{0}?page={1}&take={2}",
-                            "api/shipments", 1, 2),
+                            ApiBaseRequestPath, 1, 2),
                         mediaType: "application/json",
                         username: Constants.ValidAdminUserName,
                         password: Constants.ValidAdminPassword);
@@ -80,14 +86,83 @@ namespace PingYourPackage.API.Test.Integration.Controllers {
             public async Task
                 Returns_200_And_Shipment_If_Request_Authorized_And_Shipment_Exists() {
 
-                throw new NotImplementedException();
+                // Arrange
+                Guid[] keys = new[] { 
+                    Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid()
+                };
+                var requestKey = keys[1];
+
+                // Get the config and request
+                var configAndRequest = GetConfigAndRequestMessage(keys, requestKey);
+
+                // Act
+                var userDto = await IntegrationTestHelper
+                    .GetResponseMessageBodyAsync<ShipmentDto>(
+                        configAndRequest.Item1, 
+                        configAndRequest.Item2, 
+                        HttpStatusCode.OK);
+
+                // Assert
+                Assert.Equal(requestKey, userDto.Key);
             }
 
             [Fact, NullCurrentPrincipal]
             public async Task
                 Returns_404_If_Request_Authorized_But_Shipment_Does_Not_Exist() {
+                
+                // Arrange
+                Guid[] keys = new[] { 
+                    Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid()
+                };
+                var requestKey = Guid.NewGuid();
 
-                throw new NotImplementedException();
+                // Get the config and request
+                var configAndRequest = GetConfigAndRequestMessage(keys, requestKey);
+
+                // Act
+                var response = await IntegrationTestHelper
+                    .GetResponseAsync(
+                        configAndRequest.Item1, configAndRequest.Item2);
+
+                // Assert
+                Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+            }
+
+            private static Tuple<HttpConfiguration, HttpRequestMessage> 
+                GetConfigAndRequestMessage(Guid[] keys, Guid requestKey) {
+
+                var config = IntegrationTestHelper
+                    .GetInitialIntegrationTestConfig(GetContainer(keys));
+
+                var request = HttpRequestMessageHelper
+                    .ConstructRequest(
+                        httpMethod: HttpMethod.Get,
+                        uri: string.Format(
+                            "https://localhost/{0}/{1}",
+                            ApiBaseRequestPath, requestKey),
+                        mediaType: "application/json",
+                        username: Constants.ValidAdminUserName,
+                        password: Constants.ValidAdminPassword);
+
+                return Tuple.Create(config, request);
+            }
+
+            private static IContainer GetContainer(Guid[] keys) {
+
+                var shipments = GetDummyShipments(keys);
+                var containerBuilder = GetInitialContainerBuilder();
+
+                Mock<IShipmentService> shipmentSrvMock = new Mock<IShipmentService>();
+                shipmentSrvMock.Setup(ss => ss.GetShipment(
+                        It.IsAny<Guid>()
+                    )
+                ).Returns<Guid>(key => shipments.FirstOrDefault(x => x.Key == key));
+
+                containerBuilder.Register(c => shipmentSrvMock.Object)
+                    .As<IShipmentService>()
+                    .InstancePerApiRequest();
+
+                return containerBuilder.Build();
             }
         }
 
@@ -128,28 +203,235 @@ namespace PingYourPackage.API.Test.Integration.Controllers {
             public async Task
                 Returns_404_If_Request_Authorized_But_Shipment_Does_Not_Exist() {
 
-                throw new NotImplementedException();
+                // Arrange
+                Guid[] keys = new[] { 
+                    Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid()
+                };
+                var requestKey = Guid.NewGuid();
+                var config = IntegrationTestHelper
+                    .GetInitialIntegrationTestConfig(GetContainer(keys));
+
+                var shipmentRequestModel = new ShipmentUpdateRequestModel {
+                    ShipmentTypeKey = Guid.NewGuid(),
+                    Price = 12.23M,
+                    ReceiverName = "Receiver 1 Name",
+                    ReceiverSurname = "Receiver 1 Surname",
+                    ReceiverAddress = "Receiver 1 Address",
+                    ReceiverCity = "Receiver 1 City",
+                    ReceiverCountry = "Receiver 1 Country",
+                    ReceiverTelephone = "Receiver 1 Country",
+                    ReceiverZipCode = "12345",
+                    ReceiverEmail = "foo@example.com"
+                };
+
+                var request = HttpRequestMessageHelper
+                    .ConstructRequest(
+                        httpMethod: HttpMethod.Put,
+                        uri: string.Format(
+                            "https://localhost/{0}/{1}",
+                            ApiBaseRequestPath, requestKey),
+                        mediaType: "application/json",
+                        username: Constants.ValidAdminUserName,
+                        password: Constants.ValidAdminPassword);
+
+                request.Content = new ObjectContent<ShipmentUpdateRequestModel>(
+                    shipmentRequestModel, new JsonMediaTypeFormatter());
+
+                // Act
+                var response = await IntegrationTestHelper
+                    .GetResponseAsync(config, request);
+
+                // Assert
+                Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
             }
 
             [Fact, NullCurrentPrincipal]
             public async Task
                 Returns_400_If_Request_Shipment_But_Invalid() {
 
-                throw new NotImplementedException();
+                // Arrange
+                Guid[] keys = new[] { 
+                    Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid()
+                };
+                var requestKey = Guid.NewGuid();
+                var config = IntegrationTestHelper
+                    .GetInitialIntegrationTestConfig(GetContainer(keys));
+
+                var shipmentRequestModel = new ShipmentUpdateRequestModel {
+                    ReceiverName = "ANameWhichIsMoreThan50CharsANameWhichIsMoreThan50Chars",
+                    ReceiverSurname = "ASurnameWhichIsMoreThan50CharsASurnameWhichIsMoreThan50Chars",
+                    ReceiverAddress = "AnAddressWhichIsMoreThan50CharsAnAddressWhichIsMoreThan50Chars",
+                    ReceiverCity = "ACityWhichIsMoreThan50CharsACityWhichIsMoreThan50Chars",
+                    ReceiverTelephone = "ATelephoneWhichIsMoreThan50CharsATelephoneWhichIsMoreThan50Chars",
+                    ReceiverEmail = "fooexample.com"
+                };
+
+                var request = HttpRequestMessageHelper
+                    .ConstructRequest(
+                        httpMethod: HttpMethod.Put,
+                        uri: string.Format(
+                            "https://localhost/{0}/{1}",
+                            ApiBaseRequestPath, requestKey),
+                        mediaType: "application/json",
+                        username: Constants.ValidAdminUserName,
+                        password: Constants.ValidAdminPassword);
+
+                request.Content = new ObjectContent<ShipmentUpdateRequestModel>(
+                    shipmentRequestModel, new JsonMediaTypeFormatter());
+
+                var httpError = await IntegrationTestHelper.
+                    GetResponseMessageBodyAsync<HttpError>(
+                        config, request, HttpStatusCode.BadRequest);
+
+                var modelState = (HttpError)httpError["ModelState"];
+                var shipmentTypeKeyError = modelState["requestModel.ShipmentTypeKey"] as string[];
+                var priceError = modelState["requestModel.Price"] as string[];
+                var receiverNameError = modelState["requestModel.ReceiverName"] as string[];
+                var receiverSurnameError = modelState["requestModel.ReceiverSurname"] as string[];
+                var receiverAddressError = modelState["requestModel.ReceiverAddress"] as string[];
+                var receiverCityError = modelState["requestModel.ReceiverCity"] as string[];
+                var receiverCountryError = modelState["requestModel.ReceiverCountry"] as string[];
+                var receiverTelephoneError = modelState["requestModel.ReceiverTelephone"] as string[];
+                var receiverEmailError = modelState["requestModel.ReceiverEmail"] as string[];
+                var receiverZipCodeError = modelState["requestModel.ReceiverZipCode"] as string[];
+
+                // Assert
+                Assert.NotNull(shipmentTypeKeyError);
+                Assert.NotNull(priceError);
+                Assert.NotNull(receiverNameError);
+                Assert.NotNull(receiverSurnameError);
+                Assert.NotNull(receiverAddressError);
+                Assert.NotNull(receiverCityError);
+                Assert.NotNull(receiverCountryError);
+                Assert.NotNull(receiverTelephoneError);
+                Assert.NotNull(receiverEmailError);
+                Assert.NotNull(receiverZipCodeError);
             }
 
             [Fact, NullCurrentPrincipal]
             public async Task
                 Returns_200_And_Updated_Shipment_If_Request_Authorized_But_Request_Is_Valid() {
 
-                throw new NotImplementedException();
+                // Arrange
+                Guid[] keys = new[] { 
+                    Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid()
+                };
+                var requestKey = keys[1];
+                var shipments = GetDummyShipments(keys);
+                var shipmentSrvMock = GetShipmentSrvMock(shipments);
+                var config = IntegrationTestHelper
+                    .GetInitialIntegrationTestConfig(GetContainer(keys, shipmentSrvMock));
+
+                var shipmentRequestModel = new ShipmentUpdateRequestModel {
+                    ShipmentTypeKey = Guid.NewGuid(),
+                    Price = 12.23M,
+                    ReceiverName = "Receiver 1 Name",
+                    ReceiverSurname = "Receiver 1 Surname",
+                    ReceiverAddress = "Receiver 1 Address",
+                    ReceiverCity = "Receiver 1 City",
+                    ReceiverCountry = "Receiver 1 Country",
+                    ReceiverTelephone = "Receiver 1 Country",
+                    ReceiverZipCode = "12345",
+                    ReceiverEmail = "foobar@example.com"
+                };
+
+                var request = HttpRequestMessageHelper
+                    .ConstructRequest(
+                        httpMethod: HttpMethod.Put,
+                        uri: string.Format(
+                            "https://localhost/{0}/{1}",
+                            ApiBaseRequestPath, requestKey),
+                        mediaType: "application/json",
+                        username: Constants.ValidAdminUserName,
+                        password: Constants.ValidAdminPassword);
+
+                request.Content = new ObjectContent<ShipmentUpdateRequestModel>(
+                    shipmentRequestModel, new JsonMediaTypeFormatter());
+
+                var shipmentDto = await IntegrationTestHelper.
+                    GetResponseMessageBodyAsync<ShipmentDto>(
+                        config, request, HttpStatusCode.OK);
+
+                // Assert
+                shipmentSrvMock.Verify(
+                    ss => ss.UpdateShipment(
+                        It.IsAny<Shipment>()), Times.Once()
+                );
+
+                Assert.Equal(requestKey, shipmentDto.Key);
+                Assert.Equal(shipmentRequestModel.Price, shipmentDto.Price);
+                Assert.Equal(shipmentRequestModel.ReceiverEmail, shipmentDto.ReceiverEmail);
             }
 
             [Fact, NullCurrentPrincipal]
             public async Task
                 Returns_400_If_Request_Authorized_But_Message_Body_Is_Empty() {
 
-                throw new NotImplementedException();
+                // Arrange
+                Guid[] keys = new[] { 
+                    Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid()
+                };
+                var requestKey = Guid.NewGuid();
+                var config = IntegrationTestHelper
+                    .GetInitialIntegrationTestConfig(GetContainer(keys));
+
+                var request = HttpRequestMessageHelper
+                    .ConstructRequest(
+                        httpMethod: HttpMethod.Put,
+                        uri: string.Format(
+                            "https://localhost/{0}/{1}",
+                            ApiBaseRequestPath, requestKey),
+                        mediaType: "application/json",
+                        username: Constants.ValidAdminUserName,
+                        password: Constants.ValidAdminPassword);
+
+                // Act
+                var httpError = await IntegrationTestHelper.
+                    GetResponseMessageBodyAsync<HttpError>(
+                        config, request, HttpStatusCode.BadRequest);
+
+                var modelState = (HttpError)httpError["ModelState"];
+                var requestModelError = modelState["requestModel"] as string[];
+
+                // Assert
+                Assert.NotNull(requestModelError);
+            }
+
+            private static IContainer GetContainer(Guid[] keys) {
+
+                var shipments = GetDummyShipments(keys);
+                var shipmentSrvMock = GetShipmentSrvMock(shipments);
+
+                return GetContainer(keys, shipmentSrvMock);
+            }
+
+            private static IContainer GetContainer(
+                Guid[] keys, Mock<IShipmentService> shipmentSrvMock) {
+
+                var containerBuilder = GetInitialContainerBuilder();
+
+                containerBuilder.Register(c => shipmentSrvMock.Object)
+                    .As<IShipmentService>()
+                    .InstancePerApiRequest();
+
+                return containerBuilder.Build();
+            }
+
+            private static Mock<IShipmentService> GetShipmentSrvMock(
+                IEnumerable<Shipment> shipments) {
+
+                Mock<IShipmentService> shipmentSrvMock = new Mock<IShipmentService>();
+                shipmentSrvMock.Setup(ss => ss.GetShipment(
+                        It.IsAny<Guid>()
+                    )
+                ).Returns<Guid>(key => shipments.FirstOrDefault(x => x.Key == key));
+
+                shipmentSrvMock.Setup(ss => ss.UpdateShipment(
+                        It.IsAny<Shipment>()
+                    )
+                ).Returns<Shipment>(shipment => shipment);
+
+                return shipmentSrvMock;
             }
         }
 
@@ -174,6 +456,7 @@ namespace PingYourPackage.API.Test.Integration.Controllers {
                     ReceiverCountry = string.Format("Receiver {0} Country", i),
                     ReceiverTelephone = string.Format("Receiver {0} Country", i),
                     ReceiverZipCode = "12345",
+                    ReceiverEmail = "foo@example.com",
                     CreatedOn = DateTime.Now,
                     ShipmentType = new ShipmentType {
                         Key = shipmentTypeKeys[i],
