238fa4122c3db5277bffa6ad13d3a42f238a0560
tugberkugurlu <tugberkugurlu@gmail.com>
2012-10-29 11:25:18 +0200

    added ShipmentStatesControllerIntegrationTest

---
+++ src/test/PingYourPackage.API.Test.Integration/Controllers/ShipmentStatesControllerIntegrationTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingYourPackage.API.Test.Integration.Controllers {
+
+    public class ShipmentStatesControllerIntegrationTest {
+
+        public class General { 
+
+        }
+
+        public class GetShipmentStates { 
+        
+        }
+
+        public class PostShipmentState {
+
+        }
+    }
+}
