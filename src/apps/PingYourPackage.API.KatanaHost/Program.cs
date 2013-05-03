8b3bbf63792f7685a6418baa842a692d4576759b
tugberkugurlu <tugberkugurlu@gmail.com>
2013-05-03 22:04:18 +0300

    added the katana host option but couldn't get it running on katana.exe

---
+++ src/apps/PingYourPackage.API.KatanaHost/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingYourPackage.API.KatanaHost {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            // TODO: Open both URIs (HTTP and HTTPS).
+            // TODO: Use new ASP.NET Web API integration package. 
+            //       They have different API.
+
+            using (WebApp.Start<Startup>()) {
+
+                Console.WriteLine("Started on ");
+                Console.ReadKey();
+                Console.WriteLine("Stopping");
+            }
+        }
+    }
+}
